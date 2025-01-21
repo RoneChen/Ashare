7f8abc22c2e53a0d180583f497df8511294b73e4
Rone Chen <m13552079019@163.com>
2025-01-21 23:50:35 +0800

    finish the MA strategy test

--- Demo2.py
+++ Demo2.py
@@ -5,7 +5,7 @@ from  MyTT import *            #myTT麦语言工具函数指标库  https://gith
 # 证券代码兼容多种格式 通达信，同花顺，聚宽
 # sh000001 (000001.XSHG)    sz399006 (399006.XSHE)   sh600519 ( 600519.XSHG ) 
 
-df=get_price('000001.XSHG',frequency='1d',count=120)      #默认获取今天往前120天的日线行情
+df=get_price('512800.XSHG',frequency='1d',count=120)      #默认获取今天往前120天的日线行情
 print('上证指数日线行情\n',df.tail(5))
 
 #-------有数据了，下面开始正题 -------------
@@ -17,11 +17,18 @@ MA10=MA(CLOSE,10)                              #获取10日均线序列
 up,mid,lower=BOLL(CLOSE)                       #获取布林带指标数据
 
 #-------------------------作图显示-----------------------------------------------------------------
-import matplotlib.pyplot as plt ;  from matplotlib.ticker import MultipleLocator
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
 plt.figure(figsize=(15,8))  
-plt.plot(CLOSE,label='SHZS');    plt.plot(up,label='UP');           #画图显示 
-plt.plot(mid,label='MID');       plt.plot(lower,label='LOW');
-plt.plot(MA10,label='MA10',linewidth=0.5,alpha=0.7);
-plt.legend();         plt.grid(linewidth=0.5,alpha=0.7);    plt.gcf().autofmt_xdate(rotation=45);
+plt.plot(CLOSE,label='SHZS')
+plt.plot(up,label='UP')       #画图显示 
+plt.plot(mid,label='MID')     
+plt.plot(lower,label='LOW')
+plt.plot(MA10,label='MA10',linewidth=0.5,alpha=0.7)
+plt.legend()
+plt.grid(linewidth=0.5,alpha=0.7)
+plt.gcf().autofmt_xdate(rotation=45)
 plt.gca().xaxis.set_major_locator(MultipleLocator(len(CLOSE)/30))    #日期最多显示30个
-plt.title('SH-INDEX   &   BOLL SHOW',fontsize=20);   plt.show()
+plt.title('SH-INDEX   &   BOLL SHOW',fontsize=20)
+plt.show()
---
+++ main.py
@@ -0,0 +1,189 @@
+from Ashare import get_price
+from MyTT import *
+import matplotlib.pyplot as plt
+from tabulate import tabulate
+
+
+days = 320                      # the number of days to get the price, the income calculation is base on (days-20)
+count = 100                     # the number of stocks to buy
+strategy_5 = True               # enable the MA5 strategy
+strategy_10 = True              # enable the MA10 strategy
+strategy_20 = True              # enable the MA20 strategy
+
+def get_market(code_string):
+    market_code_dict = {
+        "600": "sh",  # 沪市A股
+        "601": "sh",  # 沪市A股
+        "603": "sh",  # 沪市A股
+        "605": "sh",  # 沪市A股
+        "000": "sz",  # 深市A股
+        "001": "sz",  # 深市A股
+        "003": "sz",  # 深市A股
+        "688": "IB",  # 科创板
+        "300": "sz",  # 创业板（旧）
+        "301": "sz",  # 创业板
+        "002": "sz",  # 中小板
+    }
+    return market_code_dict.get(code_string[:3], None) + code_string
+
+stock_id = get_market('601988')
+print(stock_id)
+
+# get the price
+df = get_price(stock_id, frequency='1d', count=days)
+
+# print(df.values[-1])          # used to check the stock correctness
+
+def MA_strategys(stock_id, df):
+    # get the close, open, high, low price
+    close = df.close.values
+    open = df.open.values
+    high = df.high.values
+    low = df.low.values
+
+    MA5 = MA(close, 5)
+    MA10 = MA(close, 10)
+    MA20 = MA(close, 20)
+
+    df['MA5'] = MA5
+    df['MA10'] = MA10
+    df['MA20'] = MA20
+
+    # preprocessing the DataFrame, which reset the index to a column.
+    df.index.name = 'date'
+    df= df.reset_index()
+    # print('中国平安\n', df.head(20))
+
+    item_20th = df.iloc[19]
+    last_MA5 = item_20th['MA5']
+    last_MA10 = item_20th['MA10']
+    last_MA20 = item_20th['MA20']
+
+    cost = item_20th['close'] * count
+
+    flag_MA5 = 1            # 0: above, 1: below
+    flag_MA10 = 1           # 0: above, 1: below
+    flag_MA20 = 1           # 0: above, 1: below
+    total_income_MA20 = 0   # total income of MA20 strategy
+    total_income_MA10 = 0   # total income of MA10 strategy
+    total_income_MA5 = 0    # total income of MA5 strategy
+
+    # Loop through each row
+    for index, row in df.tail(days-20).iterrows():
+        # print(f"{row['date']} {row['open']: >10.2f} {row['close']: >10.2f} {row['MA5']: >10.4f} {row['MA10']: >10.4f} {row['MA20']: >10.4f}")
+        
+        #------------------------- Strategy MA5-------------------------
+        if strategy_5:
+            if row['close'] > last_MA5 and flag_MA5 == 1:
+                # print(f"{row['close']: >10.2f} is above {row['MA20']: >10.2f}")
+                # print(f"MA5 buy: {row['date']}")
+                total_income_MA5 += row['close'] * count
+                flag_MA5 = 0
+            
+            if row['close'] < last_MA5 and flag_MA5 == 0:
+                # print(f"{row['close']: >10.2f} is below {row['MA20']: >10.2f}")
+                # print(f"MA5 sell: {row['date']}")
+                total_income_MA5 -= row['close'] * count
+                flag_MA5 = 1
+
+            last_MA5 = row['MA5']
+
+        #------------------------- Strategy MA10-------------------------
+        if strategy_10:
+            if row['close'] > last_MA10 and flag_MA10 == 1:
+                # print(f"{row['close']: >10.2f} is above {row['MA20']: >10.2f}")
+                # print(f"MA10 buy: {row['date']}")
+                total_income_MA10 += row['close'] * count
+                flag_MA10 = 0
+            
+            if row['close'] < last_MA10 and flag_MA10 == 0:
+                # print(f"{row['close']: >10.2f} is below {row['MA20']: >10.2f}")
+                # print(f"MA10 sell: {row['date']}")
+                total_income_MA10 -= row['close'] * count
+                flag_MA10 = 1
+
+            last_MA10 = row['MA10']
+
+        #------------------------- Strategy MA20-------------------------
+        if strategy_20:
+            if row['close'] > last_MA20 and flag_MA20 == 1:
+                # print(f"{row['close']: >10.2f} is above {row['MA20']: >10.2f}")
+                # print(f"MA20 buy: {row['date']}")
+                total_income_MA20 -= row['close'] * count
+                flag_MA20 = 0
+            
+            if row['close'] < last_MA20 and flag_MA20 == 0:
+                # print(f"{row['close']: >10.2f} is below {row['MA20']: >10.2f}")
+                # print(f"MA20 sell: {row['date']}")
+                total_income_MA20 += row['close'] * count
+                flag_MA20 = 1
+            
+            last_MA20 = row['MA20']
+    
+    return [f"{stock_id}", f"¥{total_income_MA5:.2f}",  f"¥{total_income_MA10:.2f}", f"¥{total_income_MA20:.2f}"], [f"{stock_id}", f"{(total_income_MA5*100 / cost):.2f}%",  f"{(total_income_MA10*100 / cost):.2f}%", f"{(total_income_MA20*100 / cost):.2f}%"], cost
+
+
+
+# Data to be displayed in the table
+values = []
+rois = []
+
+value, roi, cost = MA_strategys(stock_id, df)
+
+values.append(value)
+rois.append(roi)
+
+# Column headers
+headers_values = ["Stock", "MA5", "MA10", "MA20"]
+heasers_rois = ["Stock", "MA5", "MA10", "MA20"]
+
+# Create the markdown table
+markdown_table_values = tabulate(values, headers_values, tablefmt="github", numalign="right", stralign="center")
+markdown_table_rois = tabulate(rois, heasers_rois, tablefmt="github", numalign="right", stralign="center")
+
+# Print the markdown table
+print(f"(days: {days}, count: {count}, cost: ¥{cost})")
+print("\nValue")
+print(markdown_table_values)
+print("\nROI")
+print(markdown_table_rois)
+
+
+# ------------------------- Plotting the Data -------------------------
+
+# # Apply a simple moving average to smooth the data (e.g., 3-day moving average)
+# df['open_smooth'] = df['open'].rolling(window=3).mean()
+# df['high_smooth'] = df['high'].rolling(window=3).mean()
+# df['low_smooth'] = df['low'].rolling(window=3).mean()
+# df['close_smooth'] = df['close'].rolling(window=3).mean()
+# df['close'] = df['close']
+
+
+# # Plot the smoothed data (without markers)
+# plt.figure(figsize=(10, 6))
+
+# # Plot each line for smoothed open, high, low, close
+# # plt.plot(df['date'], df['open_smooth'], label='Smoothed Open')
+# # plt.plot(df['date'], df['high_smooth'], label='Smoothed High')
+# # plt.plot(df['date'], df['low_smooth'], label='Smoothed Low')
+# # plt.plot(df['date'], df['close_smooth'], label='Smoothed Close')
+# plt.plot(df['date'], df['close'], label='Close')
+# # plt.plot(df['date'], df['MA5'], label='MA5')
+# # plt.plot(df['date'], df['MA10'], label='MA10')
+# plt.plot(df['date'], df['MA20'], label='MA20')
+
+# # Add title and labels
+# plt.title('Smoothed Stock Prices over Time')
+# plt.xlabel('Date')
+# plt.ylabel('Price')
+# plt.legend(loc='best')
+
+# # Rotate date labels for better readability
+# plt.xticks(rotation=45)
+
+# # Display gridlines
+# plt.grid(True)
+
+# # Show the plot
+# plt.tight_layout()
+# plt.show()
